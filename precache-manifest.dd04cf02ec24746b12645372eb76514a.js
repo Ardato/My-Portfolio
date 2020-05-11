c94e91864491d8b9b06b431b458e0eb21aba2c69
ardato belay <aradatobelay@gmaill.com>
2020-05-11 12:09:31 +0300

    Updates

---
+++ precache-manifest.dd04cf02ec24746b12645372eb76514a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd8366843fa797cfeef7482917c14cd5",
+    "url": "/My-Portfolio/index.html"
+  },
+  {
+    "revision": "761134619d1c217901d9",
+    "url": "/My-Portfolio/static/css/main.365a177a.chunk.css"
+  },
+  {
+    "revision": "017a319ac871e93182bc",
+    "url": "/My-Portfolio/static/js/2.c8f52f6c.chunk.js"
+  },
+  {
+    "revision": "cf89bcfdc86db2c261a743301fbeb514",
+    "url": "/My-Portfolio/static/js/2.c8f52f6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "761134619d1c217901d9",
+    "url": "/My-Portfolio/static/js/main.87be730f.chunk.js"
+  },
+  {
+    "revision": "61de4b974ec97ee80e97",
+    "url": "/My-Portfolio/static/js/runtime-main.587418a8.js"
+  },
+  {
+    "revision": "2dd213aa54b22c3bca0c62f570d1512a",
+    "url": "/My-Portfolio/static/media/coffee-apple.2dd213aa.jpg"
+  },
+  {
+    "revision": "7938a9f81afa2158e0013980322a6c7d",
+    "url": "/My-Portfolio/static/media/p1.7938a9f8.png"
+  },
+  {
+    "revision": "8ee70398fdc92fd6c5e925302998dd20",
+    "url": "/My-Portfolio/static/media/p2.8ee70398.png"
+  },
+  {
+    "revision": "988029cade3771262e34f4931fe01154",
+    "url": "/My-Portfolio/static/media/p3.988029ca.png"
+  },
+  {
+    "revision": "e0d13cb6be40957581ca3f6a82e3e305",
+    "url": "/My-Portfolio/static/media/p4.e0d13cb6.png"
+  },
+  {
+    "revision": "a1937fcef6226ebeb92caa04b69fa43b",
+    "url": "/My-Portfolio/static/media/p5.a1937fce.png"
+  },
+  {
+    "revision": "bc4c15407aec0c71dea907ca19dfa6e7",
+    "url": "/My-Portfolio/static/media/p6.bc4c1540.png"
+  },
+  {
+    "revision": "5ce8c9c502c5fcf9b94e4fd6f2959855",
+    "url": "/My-Portfolio/static/media/pic.5ce8c9c5.jpg"
+  }
+]);
